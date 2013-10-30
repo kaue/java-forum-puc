3be5bb1d48067158dab3fa6330e793686a9d3040
kaue.gimenes <kaue.gimenes@855c76ed-802d-13ab-0335-26ea55f006d8>
2013-10-30 20:26:31 +0000

    Criar View de Cadastro De Tema.

--- trunk/src/C/AppController.java
+++ trunk/src/C/AppController.java
@@ -2,32 +2,35 @@ package C;
 
 import M.Forum;
 import V.MenuPrincipal;
-
+import C.CadastrarTema;
+import C.CadastrarQuestao;
 public class AppController {
 
-	private MenuPrincipal menuPrincipal;
-	
+	private MenuPrincipal vMenuPrincipal;
+
+	private CadastrarTema cCadastrarTema;
+	private CadastrarQuestao cCadastrarQuestao;
+
 	private Forum forum;
-	
+
 	public AppController(){
 		forum = new Forum();
-		menuPrincipal = new MenuPrincipal(this);
+		vMenuPrincipal = new MenuPrincipal(this);
 	}
-	
+
 	public void cadastrarTema(){
-		new CadastrarTema(forum);
-		
+		cCadastrarTema = new CadastrarTema(forum);
 	}
-	
+
 	public void cadastrarQuestao(){
-		new CadastrarQuestao(forum);
+		cCadastrarQuestao = new CadastrarQuestao(forum);
 	}
-	
+
 	public void cadastrarResposta(){
-		
+
 	}
-	
+
 	public void verForum(){
-		
+
 	}
 }
--- trunk/src/C/CadastrarQuestao.java
+++ trunk/src/C/CadastrarQuestao.java
@@ -7,15 +7,14 @@ import V.CadastroDeQuestoes;
 public class CadastrarQuestao {
 
 	Forum forum;
-	CadastroDeQuestoes cadastroDeQuestoes;
-	
+	private CadastroDeQuestoes cadastroDeQuestoes;
+
 	public CadastrarQuestao(Forum f){
 		forum = f;
 		Tema[] listaTemas = f.getListaTemas();
 		cadastroDeQuestoes = new CadastroDeQuestoes(this, listaTemas);
-		
 	}
-	
+
 	public void gravarQuestao(String temaTxt, String QuestaoTxt){
 		Tema tema = forum.buscaTema(temaTxt);
 	}
