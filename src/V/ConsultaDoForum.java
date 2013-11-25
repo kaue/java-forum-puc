e1a1e40adb881b895ccdd231a11348df7c53a373
pedro.lucas.dsc <pedro.lucas.dsc@855c76ed-802d-13ab-0335-26ea55f006d8>
2013-11-25 20:38:07 +0000

    Alteração no forum de textBox para treeView

--- src/V/ConsultaDoForum.java
+++ src/V/ConsultaDoForum.java
@@ -1,5 +1,7 @@
 package V;
 
+import javax.swing.tree.*;
+import javax.swing.JTree;
 import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
@@ -15,32 +17,35 @@ public class ConsultaDoForum extends JFrame{
 	private JComboBox comboListaTemas;
 	private JTextArea txtForum;
 	private JScrollPane scpScroll;
-
+    private JTree listaForum;
+    
 	public ConsultaDoForum(Tema[] listaTemas){
-//		//Criar Array de String para Popular ComboBox
-//		if((listaTemas == null) && (listaTemas.length == 0)){
-//			System.out.print("Aviso: Nao e possivel adicionar uma questao sem Tema.\n");
-//			return;
-//		}
 
+	    DefaultMutableTreeNode forum = new DefaultMutableTreeNode("Forum");
+	    DefaultMutableTreeNode tema = null;
+	    DefaultMutableTreeNode pergunta = null;
+	    DefaultMutableTreeNode resposta = null;
+	    
 		String textoCompletoForum = "";
-		if(listaTemas == null){
-			textoCompletoForum = "(fórum vazio)";
-		} else {
+		if(listaTemas != null){
 			for (int i=0;i<listaTemas.length;i++){
-				textoCompletoForum += "Tema " + (i+1) + ": " + listaTemas[i].getTexto() + "\n";
+				//textoCompletoForum += "Tema " + (i+1) + ": " + listaTemas[i].getTexto() + "\n";
+				tema = new DefaultMutableTreeNode(listaTemas[i].getTexto());
+			    forum.add(tema);
 				Questao[] listaQuestoes = listaTemas[i].getListaQuestoes();
-				if (listaQuestoes == null){
-					textoCompletoForum += "\t(não há questões para este tema)\n";
-				} else {
+				
+				if (listaQuestoes != null){
 					for(int i2=0;i2<listaQuestoes.length;i2++){
-						textoCompletoForum += "\tQuestão " + (i2+1) + ": " + listaQuestoes[i2].getTexto() + "\n";
+						//textoCompletoForum += "\tQuestão " + (i2+1) + ": " + listaQuestoes[i2].getTexto() + "\n";
+						pergunta = new DefaultMutableTreeNode(listaQuestoes[i2].getTexto());
+					    tema.add(pergunta);
+
 						Resposta[] listaResposta = listaQuestoes[i2].getListaRespostas();
-						if(listaResposta == null){
-							textoCompletoForum += "\t\t(não há respostas para esta questão)\n";
-						} else {
+						if(listaResposta != null){
 							for(int i3=0;i3<listaResposta.length;i3++){
-								textoCompletoForum += "\t\tResposta " + (i3+1) + ": " + listaResposta[i3].getTexto() + "\n";
+								//textoCompletoForum += "\t\tResposta " + (i3+1) + ": " + listaResposta[i3].getTexto() + "\n";
+								resposta = new DefaultMutableTreeNode(listaResposta[i3].getTexto());
+								pergunta.add(resposta);
 							}
 						}
 					}
@@ -48,6 +53,8 @@ public class ConsultaDoForum extends JFrame{
 			}
 		}
 
+		listaForum = new JTree(forum);
+		
 		//Janela
 		//setBounds(600, 400, 800, 600);
 		setBounds(600, 400, 400, 400);
@@ -61,8 +68,11 @@ public class ConsultaDoForum extends JFrame{
 		txtForum.setBounds(0, 0, 800, 600);
 		txtForum.setEditable(false);
 
+		listaForum.setBounds(0, 0, 800, 600);
+
 		//Adicionar Componentes na View.
-		add(txtForum);
+		//add(txtForum);
+		add(listaForum);
 		//Configurar Layout
 		setLayout(null);
 		setVisible(true);
